66e9871239a026e050bc7622b6fef295dfcf6192
miguelsndc <mglsndc@gmail.com>
2021-07-12 18:31:54 -0300

    feat: add light color theme

---
+++ src/styles/theme.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		backgroundSecondary: string;
+		backgroundDefault: string;
+		primaryText: string;
+		secondaryText: string;
+		tertiaryText: string;
+		primaryElement: string;
+		anotherElement: string;
+		gray1: string;
+		gray2: string;
+		gray3: string;
+		gray4: string;
+		gray5: string;
+		white: string;
+	}
+}
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,17 @@
+const theme = {
+	backgroundSecondary: '#F1F9FE',
+	backgroundDefault: '#D9E6F6',
+	primaryText: '#2E7BB4',
+	secondaryText: '#388BB0',
+	tertiaryText: '#2F4A71',
+	primaryElement: '#6F92BB',
+	anotherElement: '#5579A1',
+	gray1: '#333333',
+	gray2: '#5A5A5A',
+	gray3: '#999999',
+	gray4: '#C5C6CA',
+	gray5: '#F4F4F4',
+	white: '#FFFFFF',
+};
+
+export default theme;
