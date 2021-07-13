b4322cf4c6f1f90a7c8da8fcbadecc0abeff69c7
miguelsndc <mglsndc@gmail.com>
2021-07-13 19:35:05 -0300

    chore(Theme): modify theme type file to match changes

--- src/styles/theme.d.ts
+++ src/styles/theme.d.ts
@@ -2,18 +2,21 @@ import 'styled-components';
 
 declare module 'styled-components' {
 	export interface DefaultTheme {
-		backgroundSecondary: string;
-		backgroundDefault: string;
-		primaryText: string;
-		secondaryText: string;
-		tertiaryText: string;
-		primaryElement: string;
-		anotherElement: string;
-		gray1: string;
-		gray2: string;
-		gray3: string;
-		gray4: string;
-		gray5: string;
-		white: string;
+		backgroundSecondary: '#F1F9FE';
+		backgroundDefault: '#D9E6F6';
+		primaryText: '#2E7BB4';
+		secondaryText: '#388BB0';
+		tertiaryText: '#2F4A71';
+		quarternaryText: '#d81d99';
+		primaryElement: '#6F92BB';
+		anotherElement: '#5579A1';
+		gray1: '#333333';
+		gray2: '#5A5A5A';
+		gray3: '#999999';
+		gray4: '#C5C6CA';
+		gray5: '#F4F4F4';
+		backgroundTertiary: '#FFFFFF';
+		textSecondaryColor: '#ffffff';
+		backgroundQuarternary: '#bbcde8';
 	}
 }
