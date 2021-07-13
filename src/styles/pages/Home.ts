6df7491aefedb7d1bd4af3fd4e158c27675bc292
miguelsndc <mglsndc@gmail.com>
2021-07-13 19:36:48 -0300

    feat(Home): add profile relations wrapper

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -7,3 +7,11 @@ type ContainerProps = {
 export const GridItem = styled.div<ContainerProps>`
 	grid-area: ${p => p.gridArea};
 `;
+
+export const ProfileRelationsWrapper = styled.ul`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	list-style: none;
+	gap: 0.5rem;
+	max-height: 220px;
+`;
