7725f5d6fad2876b902d43fd3210abc4ff2c1301
miguelsndc <mglsndc@gmail.com>
2021-07-13 19:34:24 -0300

    feat(Theme): add more colors

--- src/styles/theme/light.ts
+++ src/styles/theme/light.ts
@@ -4,6 +4,7 @@ const theme = {
 	primaryText: '#2E7BB4',
 	secondaryText: '#388BB0',
 	tertiaryText: '#2F4A71',
+	quarternaryText: '#d81d99',
 	primaryElement: '#6F92BB',
 	anotherElement: '#5579A1',
 	gray1: '#333333',
@@ -11,7 +12,9 @@ const theme = {
 	gray3: '#999999',
 	gray4: '#C5C6CA',
 	gray5: '#F4F4F4',
-	white: '#FFFFFF',
+	backgroundTertiary: '#FFFFFF',
+	textSecondaryColor: '#ffffff',
+	backgroundQuarternary: '#bbcde8',
 };
 
 export default theme;
