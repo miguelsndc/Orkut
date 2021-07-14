b7e75df648084979b700bbcd0d57c9f3bb1b4af2
miguelsndc <mglsndc@gmail.com>
2021-07-14 18:25:53 -0300

    feat: add fixed menu on top of the page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-
 import { ThemeProvider } from 'styled-components';
-
 import { GlobalStyles } from '@styles/globals';
 
 import lightTheme from '@styles/theme/light';
+import Menu from '@components/Menu';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={lightTheme}>
 			<GlobalStyles />
-			<Component {...pageProps} />
+			<main>
+				<Menu githubUser={'miguelsndc'} />
+				<Component {...pageProps} />
+			</main>
 		</ThemeProvider>
 	);
 }
