65857a85dd67f8fa4902a9e778d95397ee7885ee
miguelsndc <mglsndc@gmail.com>
2021-07-14 18:40:47 -0300

    feat(Home): get current user followers and communities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,101 +1,83 @@
+import { useEffect, useState } from 'react';
+
+import CreateCommunityForm from '@components/CreateCommunityForm';
 import NostalgicIconSet from '@components/NostalgicIconSet';
 import ProfileRelations from '@components/ProfileRelations';
 import ProfileSidebar from '@components/ProfileSidebar';
 import MainGrid from '@components/MainGrid';
-import Menu from '@components/Menu';
 import Box from '@components/Box';
 
+import useCommunity from 'src/hooks/useCommunity';
+import { Follower } from 'src/types/Follower';
 import * as S from '@styles/pages/Home';
-import { useSession } from 'next-auth/client';
-import { useRouter } from 'next/dist/client/router';
-
-const friends = [
-	{
-		id: 1,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-	{
-		id: 2,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-	{
-		id: 3,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-	{
-		id: 61,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-	{
-		id: 1321321,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-	{
-		id: 43531,
-		name: 'miguelsndc',
-		imageURL: 'https://github.com/miguelsndc.png',
-	},
-];
+import api from 'src/services/api';
 
 export default function Home() {
-	const [session, loading] = useSession();
-	const router = useRouter();
+	const githubUser = 'miguelsndc';
 
-	console.log(session?.accessToken);
+	const { communities, createCommunity } = useCommunity();
+	const [followers, setFollowers] = useState<Follower[]>([]);
 
-	if (!session && !loading) router.push('/login');
+	console.log(communities);
 
-	const Home = () => {
-		return (
-			<>
-				<Menu githubUser={session.user} />
-				<MainGrid>
-					<S.GridItem className='profileArea' gridArea='profileArea'>
-						<ProfileSidebar user={session.user} />
-					</S.GridItem>
-					<S.GridItem className='welcomeArea' gridArea='welcomeArea'>
-						<Box>
-							<h1 className='title'>Bem vindo</h1> <NostalgicIconSet />
-						</Box>
-					</S.GridItem>
-					<S.GridItem
-						className='profileRelationsArea'
-						gridArea='profileRelationsArea'
-					>
-						<Box>
-							<h2 className='smallTitle'>
-								Meus Amigos <span>({friends.length})</span>
-							</h2>
-							<S.ProfileRelationsWrapper>
-								{friends.map(friend => {
-									return (
-										<ProfileRelations
-											key={friend.id}
-											name={friend.name}
-											imageURL={friend.imageURL}
-										/>
-									);
-								})}
-							</S.ProfileRelationsWrapper>
-						</Box>
-						<Box>
-							<h2 className='smallTitle'>Comunidades</h2>
-						</Box>
-					</S.GridItem>
-				</MainGrid>
-			</>
-		);
-	};
+	useEffect(() => {
+		api.get<Follower[]>(`/users/${githubUser}/followers`).then(response => {
+			const { data } = response;
+			setFollowers(data);
+		});
+	}, []);
 
 	return (
-		<>
-			{session && <Home />}
-			{loading && <h1>Carregando</h1>}
-		</>
+		<MainGrid>
+			<S.GridItem className='profileArea' gridArea='profileArea'>
+				<ProfileSidebar user={'miguelsndc'} />
+			</S.GridItem>
+			<S.GridItem className='welcomeArea' gridArea='welcomeArea'>
+				<Box>
+					<h1 className='title'>Bem vindo</h1> <NostalgicIconSet />
+				</Box>
+				<Box>
+					<h2 className='subTitle'>O que você deseja fazer ?</h2>
+					<CreateCommunityForm onCreate={createCommunity} />
+				</Box>
+			</S.GridItem>
+			<S.GridItem
+				className='profileRelationsArea'
+				gridArea='profileRelationsArea'
+			>
+				<Box>
+					<h2 className='smallTitle'>
+						Meus Amigos <span>({followers.length})</span>
+					</h2>
+					<S.ProfileRelationsWrapper>
+						{followers.slice(0, 6).map(follower => {
+							return (
+								<ProfileRelations
+									key={follower.id}
+									name={follower.login}
+									imageURL={follower.avatar_url}
+								/>
+							);
+						})}
+					</S.ProfileRelationsWrapper>
+				</Box>
+				<Box>
+					<h2 className='smallTitle'>
+						Comunidades <span>({communities.length})</span>
+					</h2>
+					<S.ProfileRelationsWrapper>
+						{communities.slice(0, 6).map(community => {
+							return (
+								<ProfileRelations
+									key={community.id}
+									name={community.name}
+									imageURL={community.imageUrl}
+								/>
+							);
+						})}
+					</S.ProfileRelationsWrapper>
+				</Box>
+			</S.GridItem>
+		</MainGrid>
 	);
 }
