2a1e6e19a7a9242c61517b8e1fa40d909b7c2328
miguelsndc <mglsndc@gmail.com>
2021-07-15 13:59:10 -0300

    chore: add support to graphql

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 module.exports = {
+	webpack: config => {
+		config.module.rules.push({
+			test: /\.(graphql|gql)$/,
+			exclude: /node_modules/,
+			loader: 'graphql-tag/loader',
+		});
+
+		return config;
+	},
+	webpackDevMiddleware: config => config,
 	images: {
-		domains: ['github.com', 'avatars.githubusercontent.com', 'pbs.twimg.com'],
+		domains: [
+			'github.com',
+			'avatars.githubusercontent.com',
+			'pbs.twimg.com',
+			'i.pinimg.com',
+		],
 	},
 };
