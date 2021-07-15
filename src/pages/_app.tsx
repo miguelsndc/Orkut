e6ac215dc8ccc103513d65ed3e2fcdd0e9201f19
miguelsndc <mglsndc@gmail.com>
2021-07-15 14:00:57 -0300

    feat: provide apollo client to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,18 @@ import { GlobalStyles } from '@styles/globals';
 
 import lightTheme from '@styles/theme/light';
 import Menu from '@components/Menu';
+import { ApolloProvider } from '@apollo/client';
+import client from 'src/config/apolloClient';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={lightTheme}>
 			<GlobalStyles />
 			<main>
-				<Menu githubUser={'miguelsndc'} />
-				<Component {...pageProps} />
+				<ApolloProvider client={client}>
+					<Menu githubUser={'miguelsndc'} />
+					<Component {...pageProps} />
+				</ApolloProvider>
 			</main>
 		</ThemeProvider>
 	);
