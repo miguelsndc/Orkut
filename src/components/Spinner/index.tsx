0c57449ac0cd62891bed7e2f83ecb39ac55fa144
miguelsndc <mglsndc@gmail.com>
2021-07-16 20:57:25 -0300

    feat: add loading spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+export default function Spinner() {
+	return (
+		<S.Spinner>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</S.Spinner>
+	);
+}
