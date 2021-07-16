f8224bfe535de6ea6d430184b555ac1aa2e2c8c9
miguelsndc <mglsndc@gmail.com>
2021-07-16 20:44:48 -0300

    feat(CreatePostForm): update layout

--- src/components/CreatePostForm/styles.ts
+++ src/components/CreatePostForm/styles.ts
@@ -1,29 +1,48 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-	textarea {
-		width: 100%;
-		resize: none;
-		min-height: 112px;
+	.profile-review {
+		display: flex;
+		align-items: flex-start;
+		gap: 0.35rem;
 
-		border: 2px solid ${({ theme }) => theme.primaryElement};
-		border-radius: 8px;
-		padding: 0.35rem;
-		font-family: inherit;
+		span {
+			display: block;
+			font-weight: 500;
+		}
 
-		&:focus,
-		&:active {
-			border-color: ${({ theme }) => theme.primaryElement};
+		span:last-child {
+			color: ${({ theme }) => theme.gray3};
+			font-size: 0.875rem;
+			margin-top: 2px;
 		}
 	}
 
+	img {
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+	}
+
+	.text {
+		width: 100%;
+
+		word-wrap: break-word;
+		resize: none;
+		min-height: 86px;
+		margin-top: 0.85rem;
+
+		outline: none;
+		border: none;
+		padding: 0.35rem;
+		font-family: inherit;
+	}
+
 	button {
+		display: block;
+		margin-left: auto;
 		margin-top: 1rem;
 		cursor: pointer;
-
-		&:hover {
-			filter: brightness(0.9);
-		}
 	}
 `;
 
