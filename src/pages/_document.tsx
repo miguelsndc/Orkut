7695e8495ce57b91863cddc13510bc9c23fcbf6b
miguelsndc <mglsndc@gmail.com>
2021-07-16 21:01:05 -0300

    feat(Document): replace fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
 						crossOrigin='true'
 					/>
 					<link
-						href='https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap'
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
 						rel='stylesheet'
 					/>
 				</Head>
