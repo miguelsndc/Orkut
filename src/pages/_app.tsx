8082491dff394eff2a6fe60a96031bce6262eb1f
miguelsndc <mglsndc@gmail.com>
2021-07-21 22:46:33 -0300

    fix: remove menu from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { Toaster } from 'react-hot-toast';
 import { GlobalStyles } from '@styles/globals';
 
 import lightTheme from '@styles/theme/light';
-import Menu from '@components/Menu';
 import { ApolloProvider } from '@apollo/client';
 import { IconContext } from 'react-icons';
 import client from 'src/config/apolloClient';
@@ -31,7 +30,6 @@ export default function App({ Component, pageProps }: AppProps) {
 						<IconContext.Provider
 							value={{ color: lightTheme.gray1, size: '1.2rem' }}
 						>
-							<Menu githubUser={'miguelsndc'} />
 							<Component {...pageProps} />
 						</IconContext.Provider>
 					</ApolloProvider>
