c9783446b4ab1b6038d9bbd03c4bd1ba95ec6bdf
miguelsndc <mglsndc@gmail.com>
2021-07-23 17:58:10 -0300

    fix(PostDetails): add under development  message

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,17 +1,8 @@
-import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/dist/client/router';
-
-import QUERY_POST_BY_ID from 'src/graphql/queries/postById.graphql';
 
 export default function PostDetails() {
 	const router = useRouter();
 	const { id } = router.query;
 
-	const { data, error, loading } = useQuery(QUERY_POST_BY_ID, {
-		variables: { id },
-	});
-
-	if (data) console.log(data);
-
-	return <div></div>;
+	return <div>PÁGINA EM CONSTRUÇÃO</div>;
 }
