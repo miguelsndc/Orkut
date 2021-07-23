8cff8751959fe70591a7616964ebe11545f7c25f
miguelsndc <mglsndc@gmail.com>
2021-07-23 17:47:24 -0300

    feat(Post): add like & dislike

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -2,9 +2,11 @@ import Box from '@components/Box';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Container } from './styles';
+import { IoThumbsDownSharp, IoThumbsUpSharp } from 'react-icons/io5';
 
 import { parseISO, format } from 'date-fns';
 import PTBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 type Author = {
 	name: string;
@@ -17,6 +19,8 @@ type Post = {
 	content: string;
 	author: Author;
 	createdAt: string;
+	likes: number;
+	dislikes: number;
 };
 
 type PostProps = {
@@ -27,6 +31,31 @@ export default function Post({ post }: PostProps) {
 	const formattedDate = format(createdAt, "dd 'de' MMMM', às ' HH:mm'h'", {
 		locale: PTBR,
 	});
+
+	const [likes, setLikes] = useState(post.likes);
+	const [dislikes, setDislikes] = useState(post.dislikes);
+	const [hasLiked, setHasLiked] = useState(false);
+	const [hasDisliked, setHasDisliked] = useState(false);
+
+	function handleLike() {
+		if (!hasLiked) {
+			setLikes(prevLikes => prevLikes + 1);
+			setHasLiked(true);
+		} else {
+			setLikes(prevLikes => prevLikes - 1);
+			setHasLiked(false);
+		}
+	}
+
+	function handleDislike() {
+		if (!hasDisliked) {
+			setDislikes(prevDislikes => prevDislikes + 1);
+			setHasDisliked(true);
+		} else {
+			setDislikes(prevDislikes => prevDislikes - 1);
+			setHasDisliked(false);
+		}
+	}
 
 	return (
 		<Container>
@@ -48,6 +77,16 @@ export default function Post({ post }: PostProps) {
 				<p>{post.content}</p>
 				<hr />
 				<div className='options'>
+					<div className='wrapper'>
+						<button className='likes' onClick={handleLike}>
+							<IoThumbsUpSharp />
+							<span>{likes}</span>
+						</button>
+						<button className='dislikes' onClick={handleDislike}>
+							<IoThumbsDownSharp />
+							<span>{dislikes}</span>
+						</button>
+					</div>
 					<Link href={`/posts/${post.id}`}>Responder</Link>
 				</div>
 			</Box>
