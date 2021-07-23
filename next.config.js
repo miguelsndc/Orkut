049a18847914f2ced545c64026b5560d8608d70f
miguelsndc <mglsndc@gmail.com>
2021-07-23 17:59:10 -0300

    chore: update allowed domains & remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 module.exports = {
-	webpack: config => {
-		config.module.rules.push({
-			test: /\.(graphql|gql)$/,
-			exclude: /node_modules/,
-			loader: 'graphql-tag/loader',
-		});
-
-		return config;
-	},
-	webpackDevMiddleware: config => config,
 	images: {
 		domains: [
 			'github.com',
 			'avatars.githubusercontent.com',
-			'pbs.twimg.com',
-			'i.pinimg.com',
-			'www.datocms-assets.com',
+			'firebasestorage.googleapis.com',
 		],
 	},
 };
