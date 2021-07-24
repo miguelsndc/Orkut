afee0a6379faf8a112160e38290177ecdbeefb70
miguelsndc <mglsndc@gmail.com>
2021-07-24 17:42:25 -0300

    chore: update post type

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,10 +1,15 @@
+import { firebase } from 'src/services/firebase/config';
 import { Author } from './Author';
 
 export type PostType = {
 	id: string;
 	content: string;
 	author: Author;
-	createdAt: string;
+	createdAt:
+		| firebase.firestore.Timestamp
+		| firebase.firestore.FieldValue
+		| string
+		| Date;
 	likes: number;
 	dislikes: number;
 };
