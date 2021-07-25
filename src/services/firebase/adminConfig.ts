99f7487459cfb7337716810fb4f9e0d054947f4d
miguelsndc <mglsndc@gmail.com>
2021-07-24 22:32:03 -0300

    fix(adminConfig): private key parse error

--- src/services/firebase/adminConfig.ts
+++ src/services/firebase/adminConfig.ts
@@ -3,7 +3,7 @@ import * as firebaseAdmin from 'firebase-admin';
 if (!firebaseAdmin.apps.length) {
 	firebaseAdmin.initializeApp({
 		credential: firebaseAdmin.credential.cert({
-			privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+			privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
 			clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
 			projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
 		}),
